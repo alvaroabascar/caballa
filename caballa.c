50f79db2023e8e3071a1872aec4a2caa9f936189
alvaro <alvaroabascar@gmail.com>
2015-12-15 00:14:52 +0100

    full refactor, bugs

--- caballa.c
+++ caballa.c
@@ -30,6 +30,7 @@ void add_history(char *unused) {}
 #include <editline/history.h>
 #endif
 
+/* Macros */
 #define min(a, b) ((a > b) ? b : a)
 #define max(a, b) ((a < b) ? b : a)
 
@@ -45,6 +46,10 @@ typedef struct lval {
     struct lval **cell;
 } lval;
 
+/* Prototypes */
+void lval_print(lval *v);
+lval* lval_add(lval *v, lval *x);
+
 
 /* possible lval types
  * LVAL_ERR: an error
@@ -54,7 +59,7 @@ typedef struct lval {
  *             variable length list of other values.
  *             (http://www.buildyourownlisp.com/chapter9_s_expressions)
  */
-enum { LVAL_ERR, LLVAL_NUM, LVAL_SYM, LVAL_SEXPR };
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
 
 /* construct a pointer to a new Number lval */
 lval* lval_num(long x)
@@ -123,74 +128,127 @@ void lval_del(lval *v)
     free(v);
 }
 
+lval* lval_read_num(mpc_ast_t *t)
+{
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err("invalid number");
+}
+
+lval* lval_read(mpc_ast_t *t)
+{
+    /* If Symbol or Number return conversion to that type */
+    if (strstr(t->tag, "number")) { return lval_read_num(t); }
+    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+    /* If root (>) or sexpr then create empty list */
+    lval *x = NULL;
+    if ((strcmp(t->tag, ">") == 0) ||
+        (strstr(t->tag, "sexpr"))) {
+        x = lval_sexpr();
+    }
+
+    /* Fill this list with any valid expression contained within */
+    for (int i = 0; i < t->children_num; i++) {
+        if ((strcmp(t->children[i]->contents, "(") == 0) ||
+            (strcmp(t->children[i]->contents, ")") == 0) ||
+            (strcmp(t->children[i]->contents, "}") == 0) ||
+            (strcmp(t->children[i]->contents, "{") == 0) ||
+            (strcmp(t->children[i]->tag, "regex") == 0)) {
+            x = lval_add(x, lval_read(t->children[i]));
+        }
+    }
+    return x;
+}
+
+lval* lval_add(lval *v, lval *x)
+{
+    v->count++;
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    return v;
+}
+
+
 
 void print_error(char *msg)
 {
     printf("Error: %s\n", msg);
 }
 
-/* print a lval */
-void lval_print(lval v)
+void lval_expr_print(lval *v, char open, char close)
 {
-    switch(v.type) {
-        /* lval is a number? print it */
-        case LVAL_NUM:
-            printf("%li\n", v.num);
-            break;
+    putchar(open);
+    for (int i = 0; i < v->count; i++) {
+        /* Print Value contained within */
+        lval_print(v->cell[i]);
+        /* Don't print trailing space if last element */
+        if (i != (v->count - 1)) {
+            putchar(' ');
+        }
+    }
+    putchar(close);
+}
 
-        /* lval is an error? check type, print it */
+
+void lval_print(lval *v)
+{
+    switch(v->type) {
+        case LVAL_NUM:
+            printf("%li\n", v->num);
+            break;
         case LVAL_ERR:
-            if (v.err == LERR_DIV_ZERO)
-                print_error("Division by zero!");
-            if (v.err == LERR_BAD_OP)
-                print_error("Invalid symbol!");
-            if (v.err == LERR_BAD_NUM)
-                print_error("Invalud number!");
+            printf("Error: %s", v->err);
+            break;
+        case LVAL_SYM:
+            printf("%s", v->sym);
+            break;
+        case LVAL_SEXPR:
+            lval_expr_print(v, '(', ')');
             break;
     }
 }
 
 /* print a lval followed by a newline */
-void lval_println(lval v)
+void lval_println(lval *v)
 {
     lval_print(v);
     putchar('\n');
 }
 
 /* Use symbol string to see which operation to perform, perform it. */
-lval eval_op(lval x, char *op, lval y)
+lval *eval_op(lval *x, char *op, lval *y)
 {
     /* if either value is an error, return it */
-    if (x.type == LVAL_ERR) { return x; }
-    if (y.type == LVAL_ERR) { return y; }
+    if (x->type == LVAL_ERR) { return lval_err(x->err); }
+    if (y->type == LVAL_ERR) { return lval_err(y->err); }
 
     /* otherwise do maths on the numbers, create and return a lval of type number */
-    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
-    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
-    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
-    if (strcmp(op, "%") == 0) { return lval_num(x.num % y.num); }
+    if (strcmp(op, "+") == 0) { return lval_num(x->num + y->num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x->num - y->num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x->num * y->num); }
+    if (strcmp(op, "%") == 0) { return lval_num(x->num % y->num); }
     if (strcmp(op, "/") == 0) {
-        return (y.num == 0) ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+        return (y->num == 0) ? lval_err("division by zero") : lval_num(x->num / y->num);
     }
     
-    return lval_err(LERR_BAD_OP);
+    return lval_err("invalid number");
 }
 
 /* Evaluate the Abstract Syntax Tree */
-lval eval(mpc_ast_t *t)
+lval* eval(mpc_ast_t *t)
 {
     /* If it is a number return it */
     if (strstr(t->tag, "number")) {
         errno = 0;
         long x = strtol(t->contents, NULL, 10);
-        return errno != ERANGE ? lval_num(x): lval_err(LERR_BAD_NUM);
+        return errno != ERANGE ? lval_num(x): lval_err("invalid number");
     }
 
     /* The symbol is always second child (first is '(') */
     char *op = t->children[1]->contents;
 
     /* We store the third child in x */
-    lval x = eval(t->children[2]);
+    lval *x = eval(t->children[2]);
 
     /* Iterate the remaining children and combining. */
     int i;
@@ -225,7 +283,8 @@ int main(int argc, char *argv[])
     puts("Press Ctrl+c to Exit\n");
 
     mpc_result_t r;
-    /* In a never ending loop */
+    lval *x;
+    /* Main loop */
     while (1) {
         /* Output our prompt and get input */
         char *input = readline("caballa> ");
@@ -236,8 +295,9 @@ int main(int argc, char *argv[])
         /* Attempt to parse the user input */
         if (mpc_parse("<stdin>", input, Caballa, &r)) {
             /* On success print the result of evaluation */
-            lval_println(eval(r.output));
-            mpc_ast_delete(r.output);
+            x = lval_read(r.output);
+            lval_println(x);
+            lval_del(x);
         } else {
             /* Otherwise print the error */
             mpc_err_print(r.error);
